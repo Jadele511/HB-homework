6d7dc5dbcc5e1e9069e4bbd590ba3ee9232d9b5c
Jade Le <baongocle1991@gmail.com>
2020-10-05 21:31:14 -0700

    accounting scripts

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -1,18 +1,14 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons_dict
 
-
-def print_melon(name, seedless, price):
+def print_melon(melons_dict):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon, attrs in melons_dict.items():
+        print(f'{melon.upper()}')
+        for attr, val in attrs.items():
+            print(f'{attr} {val}')
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+print_melon(melons_dict)
--- accounting-scripts/melons.py
+++ accounting-scripts/melons.py
@@ -1,23 +1,41 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
+
+
+melons_dict = {
+    'Honeydew' : {
+        'seedless': True,
+        'price': 0.99,
+        'flesh_color': 'green',
+        'weight': None,
+        'rind_color': 'green',
+    },
+    'Crenshaw' : {
+        'seedless': False,
+        'price': 2.00,
+        'flesh_color': 'yellow',
+        'weight': None,
+        'rind_color': 'yellow',
+    },
+    'Crane' : {
+        'seedless': False,
+        'price': 2.50,
+        'flesh_color': 'yellow',
+        'weight': None,
+        'rind_color': 'yellow'
+    },
+    'Casaba' : {
+        'seedless': False,
+        'price': 2.50,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+    'Cantaloupe' : {
+        'seedless': False,
+        'price': 2.50,
+        'flesh_color': 'yellow',
+        'weight': None,
+        'rind_color': 'green'
+    }
+
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
